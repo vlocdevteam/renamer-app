c2b5e13c6c0b4b4ead621be6ac22e987e87d08ad
Valk Castellani <valkcastellani@gmail.com>
2022-05-03 22:12:33 -0300

    Inicio da construção do formulário para renomear

--- RenamerApp.Designer.cs
+++ RenamerApp.Designer.cs
@@ -31,6 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RenamerApp));
             this.btnRenomear = new System.Windows.Forms.Button();
             this.btnFechar = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.barraProgresso = new System.Windows.Forms.ProgressBar();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnRenomear
@@ -38,23 +48,80 @@
             resources.ApplyResources(this.btnRenomear, "btnRenomear");
             this.btnRenomear.Name = "btnRenomear";
             this.btnRenomear.UseVisualStyleBackColor = true;
+            this.btnRenomear.Click += new System.EventHandler(this.btnRenomear_Click);
             // 
             // btnFechar
             // 
             resources.ApplyResources(this.btnFechar, "btnFechar");
             this.btnFechar.Name = "btnFechar";
             this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajudaToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sobreToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            resources.ApplyResources(this.ajudaToolStripMenuItem, "ajudaToolStripMenuItem");
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            resources.ApplyResources(this.sobreToolStripMenuItem, "sobreToolStripMenuItem");
+            this.sobreToolStripMenuItem.Click += new System.EventHandler(this.sobreToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.btnOpenFolder);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // barraProgresso
+            // 
+            resources.ApplyResources(this.barraProgresso, "barraProgresso");
+            this.barraProgresso.Name = "barraProgresso";
+            // 
+            // btnOpenFolder
+            // 
+            resources.ApplyResources(this.btnOpenFolder, "btnOpenFolder");
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
             // 
             // RenamerApp
             // 
             this.AcceptButton = this.btnRenomear;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.barraProgresso);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnFechar);
             this.Controls.Add(this.btnRenomear);
+            this.Controls.Add(this.menuStrip1);
             this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "RenamerApp";
+            this.Load += new System.EventHandler(this.RenamerApp_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -62,6 +129,14 @@
 
         private System.Windows.Forms.Button btnRenomear;
         private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar barraProgresso;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
     }
 }
 
--- RenamerApp.cs
+++ RenamerApp.cs
@@ -16,5 +16,36 @@ namespace RenamerApp
         {
             InitializeComponent();
         }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox sobre = new AboutBox();
+            sobre.ShowDialog();
+        }
+
+        private void btnRenomear_Click(object sender, EventArgs e)
+        {
+            barraProgresso.Maximum = 1000;
+            barraProgresso.Value = 0;
+            for(int i = 0; i < barraProgresso.Maximum; i++)
+            {
+                barraProgresso.Value++;
+            }
+        }
+
+        private void RenamerApp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+        }
     }
 }
